fb1f5d111d57740c13e173e47b8978606b36fe59
Visitha Nirmal Rajapaksha <visitha2001@gmail.com>
2025-12-07 10:35:48 +0530

    react-bits-setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <section>
+      <h1 className='text-center'>The Hub For Every Dev <br /> Events You Can&apos;t Miss</h1>
+      <p className='text-center mt-5'>Hackethons, Meetups, Webinars, All in One Place</p>
+    </section>
   )
 }
 
