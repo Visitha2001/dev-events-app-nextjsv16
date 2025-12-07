6aff8c5ba872ac700c8bd3b0dace42f413b1beab
Visitha Nirmal Rajapaksha <visitha2001@gmail.com>
2025-12-07 11:01:44 +0530

    nav and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ExploreBtn from '@/components/ExploreBtn'
 import React from 'react'
 
 const page = () => {
@@ -5,6 +6,18 @@ const page = () => {
     <section>
       <h1 className='text-center'>The Hub For Every Dev <br /> Events You Can&apos;t Miss</h1>
       <p className='text-center mt-5'>Hackethons, Meetups, Webinars, All in One Place</p>
+      <ExploreBtn />
+
+      <div className='mt-20 space-y-7'>
+        <h3>Featured Events</h3>
+        <ul className='events'>
+          {[1, 2, 3, 4, 5].map((event, index) => (
+            <li key={index}>
+              Event {event}
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
